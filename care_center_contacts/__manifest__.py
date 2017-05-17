5f9319d645073611d20a232c40613b4a2a1b7610
Dave Burkholder <dave@compassfoundation.io>
2017-05-17 16:33:37 -0400

    Add Phone Calls & Notes to Contacts menu

---
+++ care_center_contacts/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "care_center_contacts",
+
+    'summary': """
+        Extend Contacts features for Care Center.""",
+
+    'description': """
+    """,
+
+    'author': "Dave Burkholder <dave@thinkwelldesigns.com>",
+    'website': "http://www.thinkwelldesigns.com",
+
+    'category': 'Tools',
+    'version': '0.1',
+
+    'depends': [
+        'contacts',
+        'crm_phonecall',
+    ],
+
+    'data': [
+        'views/contacts.xml',
+    ],
+}
