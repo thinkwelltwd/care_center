39c3d17dc3fad19de79870b9382ad569ebb0af74
Jerrod Martin <jerrod@compassfoundation.io>
2019-07-24 16:09:09 -0400

    Call the correct function

--- care_center_assignment/wizard/reassign_task.py
+++ care_center_assignment/wizard/reassign_task.py
@@ -145,6 +145,6 @@ class ReassignTaskWizard(models.TransientModel):
         )
 
         self.notify_partner_email()
-        self.task_id.message_subscribe([self.assigned_to.id, ])
+        self.task_id.message_subscribe_users([self.assigned_to.id, ])
 
         return True
