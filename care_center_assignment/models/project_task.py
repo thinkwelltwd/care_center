1c9565aa98106f7871764014283011a6f7b70f9d
Dave Burkholder <dave@compassfoundation.io>
2019-10-08 13:31:58 -0400

    Add a human-friendly assignment message for email templates

--- care_center_assignment/models/project_task.py
+++ care_center_assignment/models/project_task.py
@@ -10,8 +10,24 @@ class ProjectTask(models.Model):
                                      ondelete='cascade', required=False)
 
     assignment_count = fields.Integer(compute='_assignment_count')
+    assignment_message = fields.Char(compute='_assignment_message',
+                                     help='Assignment description to include in email templates.')
 
     @api.multi
     def _assignment_count(self):
         for task in self:
             task.assignment_count = len(task.assignment_ids)
+
+    @api.multi
+    def _assignment_message(self):
+        for task in self:
+            if task.user_id:
+                uname = task.user_id.name
+                if not task.team_id:
+                    task.assignment_message = "%s's queue" % uname
+                else:
+                    task.assignment_message = "%s from the %s team" % (uname, task.team_id.name)
+            elif task.team_id:
+                task.assignment_message = 'the %s team' % task.team_id.name
+            else:
+                task.assignment_message = 'the general queue'
