4a808cad8d06f640912aa856da5762d50b387429
Denver Risser <denver@compassfoundation.io>
2020-08-24 14:13:17 -0400

    Fix qty_delivered & qty_invoiced .01 rounding mismatch for timesheets

--- care_center_timesheets/models/sale_order.py
+++ care_center_timesheets/models/sale_order.py
@@ -88,3 +88,28 @@ class SaleOrderLine(models.Model):
             ]
 
         return super(SaleOrderLine, self)._compute_analytic(domain=domain)
+
+    @api.depends('invoice_lines.invoice_id.state', 'invoice_lines.quantity')
+    def _get_invoice_qty(self):
+        """
+        Override superclass in /odoo/addons/sale/modules/sale.py (around line 1023) to round 'HALF-UP' on timesheet
+        lines to match _get_delivered_quantity_by_analytic() (same file around line 1294).
+        """
+        print('in the overridden _get_invoice_qty')
+        for line in self:
+            if line.qty_delivered_method != 'timesheet':
+                super()._get_invoice_qty()
+                continue
+
+            qty_invoiced = 0.0
+            for invoice_line in line.invoice_lines:
+                if invoice_line.invoice_id.state != 'cancel':
+                    print(f'round by HALF-UP on UOM {invoice_line.uom_id.name}')
+                    line_rounded = invoice_line.uom_id._compute_quantity(
+                        invoice_line.quantity, line.product_uom, rounding_method='HALF-UP'
+                    )
+                    if invoice_line.invoice_id.type == 'out_invoice':
+                        qty_invoiced += line_rounded
+                    elif invoice_line.invoice_id.type == 'out_refund':
+                        qty_invoiced -= line_rounded
+            line.qty_invoiced = qty_invoiced
