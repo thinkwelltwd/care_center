b5b59541310d8e329baf56d93dbaac0dfb4290ba
Dan Tillinghast <dan@compassfoundation.io>
2022-04-27 17:08:05 -0400

    Remove migration scripts from previous version

--- care_center_timesheets/migrations/10.0.4.0.0/post-invoice_status.py
+++
@@ -1,5 +0,0 @@
-def migrate(cr, version):
-    if not version:
-        return
-    cr.execute('UPDATE account_analytic_line SET invoice_status=temporary_invoice_status')
-    cr.execute('ALTER TABLE account_analytic_line DROP COLUMN temporary_invoice_status')
