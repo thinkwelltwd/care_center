b774a4cdd932bc9f661f1662b3ed8b3ed80bfce1
Josh Burkholder <joshua@compassfoundation.io>
2022-07-08 15:11:12 -0400

    Clean up care_center_hr

--- care_center_hr/__manifest__.py
+++ care_center_hr/__manifest__.py
@@ -1,23 +1,14 @@
 {
-    'name': "care_center_hr",
-
-    'summary': """
-        Extend HR features for Care Center.""",
-
-    'description': """
-     """,
-
+    'name': "Care Center HR",
+    'summary': """Extend HR features for Care Center.""",
     'author': "Dave Burkholder <dave@thinkwelldesigns.com>",
     'website': "http://www.thinkwelldesigns.com",
-
     'category': 'Human Resources',
     'version': '14.0.1.0.0',
-
     'depends': [
         'care_center',
         'hr_timesheet_attendance',
     ],
-
     'data': [
         'report/hr_attendance.xml',
         'report/hr_timesheet_attendance.xml',
