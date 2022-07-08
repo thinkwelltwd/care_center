db0f86c1cedc3fa7fd03155a5e402e026ed66620
Josh Burkholder <joshua@compassfoundation.io>
2022-07-08 16:10:50 -0400

    Remove care_center_mattermost

--- care_center_mattermost/mattermost/__init__.py
+++
@@ -1,2 +0,0 @@
-from .client import Client
-from .exceptions import *
--- care_center_mattermost/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import basemodel
-from . import ir_mail_server
-from . import mail_mail
-from . import res_partner
-from . import mail_compose_message
