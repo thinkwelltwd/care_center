d088766d8db14a4068abb0bf781b31c90d64079b
Josh Burkholder <joshua@compassfoundation.io>
2023-05-04 18:20:52 -0400

    Fix migration script

--- care_center/migrations/14.0.1.0.0/post-migration.py
+++ care_center/migrations/14.0.1.0.0/post-migration.py
@@ -2,7 +2,7 @@ from openupgradelib import openupgrade
 
 
 def drop_unused_field(env):
-    openupgrade.drop_columns(env.cr, 'project_task', 'parent_task_id')
+    openupgrade.drop_columns(env.cr, [('project_task', 'parent_task_id')])
 
 
 @openupgrade.migrate()
